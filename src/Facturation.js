50e7fe651cb2a92ed5d7dfc3c41a161f83ff1c09
chloy <buffshloy55@gmail.com>
2020-06-11 05:53:51 +0200

    media query

--- src/Facturation.js
+++ src/Facturation.js
@@ -7,15 +7,13 @@ function Facturation (props) {
      <>  
     <h4>Facturation</h4>
     <div className='containerFacturation'>
-        <div>
+        <div style={{width:'100%'}}>
             <h6>Article</h6>    
             <input type='text' onChange={(e) => props.handlearticle(e.target.value)} placeholder=' Baby Boumer' className='facturationInput' />
-        </div>
-        <div>
+      
             <h6>Quantité</h6>    
             <input type='number' onChange={(e) => props.handlequantité(e.target.value)} placeholder=' 1' className='facturationInput' />
-        </div>
-        <div>
+  
             <h6>prix</h6>    
             <input type='number' onChange={(e) => props.handleprix(e.target.value)} placeholder=' 70₪' className='facturationInput'/>
         </div>
