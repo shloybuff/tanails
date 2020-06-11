f8102c55efef7146e186ee31a02551e6e8716887
chloy <buffshloy55@gmail.com>
2020-06-11 05:38:58 +0200

    Updates

---
+++ precache-manifest.6107cc90e3301c965a19d66401d2cf22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7a1d5e8acc4e2a7d0962928997ca27",
+    "url": "/tanails/index.html"
+  },
+  {
+    "revision": "4943f3c96839cf74994b",
+    "url": "/tanails/static/css/main.65042aef.chunk.css"
+  },
+  {
+    "revision": "8626f9af7effe4c7f14d",
+    "url": "/tanails/static/js/2.81147f9a.chunk.js"
+  },
+  {
+    "revision": "86ee6800bb9113f380bcdd210b299916",
+    "url": "/tanails/static/js/2.81147f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4943f3c96839cf74994b",
+    "url": "/tanails/static/js/main.8e1f7c50.chunk.js"
+  },
+  {
+    "revision": "a2b5d4a367f026811800",
+    "url": "/tanails/static/js/runtime-main.843759a3.js"
+  }
+]);
